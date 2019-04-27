5044270561bcfe6df6929436faf92e75f857f138
student20 <student20@pcoip.ki.agh.edu.pl>
2019-04-27 10:32:10 +0200

    logging in and out implemented

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+	<h1>MWO</h1>
+	<div v-if="logged==false">
+		<p>Email:</p>
+		<input type="email" v-model="email">
+		<p>Password:</p>
+		<input type="password" v-model="password">
+		<br></br>
+		<button @click="logged=true">Zaloguj</button>
+	</div>
+	<div v-else>
+		<p>Witaj {{email}}</p>
+		<br></br>
+		<button @click="logout()">Wyloguj</button>
+	</div>
+	
+</div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+	data() {
+		  return {
+		    email: '',
+		    password: '',
+		    logged: false
+		    };
+		},
+		methods: {
+			  alertMyEmail() {
+			    alert(this.email);
+			  },
+			  logout(){
+				  this.email = '',
+				  this.password = '',
+				  this.logged = false
+			  }
+			}
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
