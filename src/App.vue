7189cc681ba8fe8055b131dc49cb9737fb1a2e0a
student20 <student20@pcoip.ki.agh.edu.pl>
2019-04-27 11:47:18 +0200

    first component extracted

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,14 @@
 <template>
-<div id="app">
-	<h1>Welcome to signing system</h1>
-	<div v-if="logged==false">
-		<p>Email:</p>
-		<input type="email" v-model="email">
-		<p>Password:</p>
-		<input type="password" v-model="password">
-		<br></br>
-		<div v-if="email.length > 0 && password.length > 0">
-			<button @click="checkEmailAndLogIn(email)">Log in</button>
-		</div>	
-	</div>
-	<div v-else>
-		<p>Hello {{email}}</p>
-		<br></br>
-		<button @click="logout()">Log out</button>
-	</div>
-	
+<div>
+<login-form @login="logMeIn($event)"></login-form>
 </div>
 </template>
-
 <script>
-import "milligram";
-
-export default {
-	data() {
-		  return {
-		    email: '',
-		    password: '',
-		    logged: false
-		    };
-		},
-		methods: {
-			  alertMyEmail() {
-			    alert(this.email);
-			  },
-			  logout(){
-				  this.email = '',
-				  this.password = '',
-				  this.logged = false
-			  },
-			  validEmail: function (email) {
-			      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-			      return re.test(email);
-			    },
-			    checkEmailAndLogIn(email){
-			    	if(this.validEmail(email)){
-			    		this.logged = true;
-			    	}
-			    	else{
-			    		alert(this.email + " is wrong email!" )
-			    		return false;
-			    	}
-			    }
-			}
+import LoginForm from "./LoginForm";
+export default{
+	components: {LoginForm}
 }
+
 </script>
 
 <style>
